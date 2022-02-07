efc3766ff1e31fc8ef890c69e6e0783611263802
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-02-07 20:24:34 +0900

    [ Ultimate Rust Crash Course ] Initial project with Cargo command  and

---
+++ Ultimate Rust Crash Course/Course/hello/target/debug/deps/hello-f7f618b28c011f5c.d
@@ -0,0 +1,5 @@
+/Users/0417taehyun/Documents/Study/Udemy/Ultimate Rust Crash Course/Course/hello/target/debug/deps/hello-f7f618b28c011f5c: src/main.rs
+
+/Users/0417taehyun/Documents/Study/Udemy/Ultimate Rust Crash Course/Course/hello/target/debug/deps/hello-f7f618b28c011f5c.d: src/main.rs
+
+src/main.rs:
---
+++ Ultimate Rust Crash Course/Course/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/0417taehyun/Documents/Study/Udemy/Ultimate\ Rust\ Crash\ Course/Course/hello/target/debug/hello: /Users/0417taehyun/Documents/Study/Udemy/Ultimate\ Rust\ Crash\ Course/Course/hello/src/main.rs
